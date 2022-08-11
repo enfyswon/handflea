1cef817591d7af4b6f2b9d1e14ce07f5a7df405e
Beom <107173113+BeomAB9@users.noreply.github.com>
2022-08-12 08:02:32 +0900

    1
    1

---
+++ handflea/src/main/java/kr/co/handflea/MemberDAOImpl.java
@@ -0,0 +1,17 @@
+package kr.co.handflea;
+
+import javax.inject.Inject;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+
+public class MemberDAOImpl implements MemberDAO {
+
+@Inject
+SqlSession sqlSession; 
+
+@Override
+public String loginCheck(MemberDTO dto) {
+return sqlSession.selectOne("member.login_check", dto);
+ }
+}
---
+++ handflea/src/main/java/kr/co/handflea/MemberDTO.java
@@ -0,0 +1,54 @@
+package kr.co.handflea;
+
+public class MemberDTO {
+
+	private String mem_name;
+	private String mem_email;
+	private String mem_pwd;
+	private String mem_add_1;
+	private String mem_add_2;
+	private String mem_add_3;
+	private String pnum;
+	private String getMem_name() {
+		return mem_name;
+	}
+	private void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	private String getMem_email() {
+		return mem_email;
+	}
+	private void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	private String getMem_pwd() {
+		return mem_pwd;
+	}
+	private void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+	private String getMem_add_1() {
+		return mem_add_1;
+	}
+	private void setMem_add_1(String mem_add_1) {
+		this.mem_add_1 = mem_add_1;
+	}
+	private String getMem_add_2() {
+		return mem_add_2;
+	}
+	private void setMem_add_2(String mem_add_2) {
+		this.mem_add_2 = mem_add_2;
+	}
+	private String getMem_add_3() {
+		return mem_add_3;
+	}
+	private void setMem_add_3(String mem_add_3) {
+		this.mem_add_3 = mem_add_3;
+	}
+	private String getPnum() {
+		return pnum;
+	}
+	private void setPnum(String pnum) {
+		this.pnum = pnum;
+	}
+}
