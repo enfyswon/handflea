f637edbee413d4ed9dd7c371567f2fc68c89fba6
Beom <107173113+BeomAB9@users.noreply.github.com>
2022-08-21 16:14:28 +0900

    error
    1

--- handflea/src/main/java/kr/co/handflea/QnA/QnADTO.java
+++ handflea/src/main/java/kr/co/handflea/QnA/QnADTO.java
@@ -2,4 +2,56 @@ package kr.co.handflea.QnA;
 
 public class QnADTO {
 
+	private String QnA_no;
+	private String title;
+	private String mem_no;
+	private String mid;
+	private String contents;
+	private String view_cnt;
+	private String write_date;
+	public String getQnA_no() {
+		
+		
+		return QnA_no;
+	}
+	public void setQnA_no(String qnA_no) {
+		QnA_no = qnA_no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMem_no() {
+		return mem_no;
+	}
+	public void setMem_no(String mem_no) {
+		this.mem_no = mem_no;
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getView_cnt() {
+		return view_cnt;
+	}
+	public void setView_cnt(String view_cnt) {
+		this.view_cnt = view_cnt;
+	}
+	public String getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(String write_date) {
+		this.write_date = write_date;
+	}
+
 }
--- handflea/src/main/java/kr/co/handflea/QnA/QnAService.java
+++ handflea/src/main/java/kr/co/handflea/QnA/QnAService.java
@@ -1,5 +1,67 @@
 package kr.co.handflea.QnA;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.handflea.util.dto.SearchDTO;
+
+@Service
 public class QnAService {
+	
+	@Autowired
+	private QnADAO dao;
+	
+	public int update( QnADTO dto) {
+		int successCount = 0;
+		successCount = dao.update( dto );
+		return successCount;
+	}
+	
+	public int delete( QnADTO dto) {
+		int successCount = 0;
+		successCount = dao.delete( dto );
+		return successCount;
+	
+	}
+	public QnADTO detail( String QnA_no) {
+		dao.incrementViewCnt( QnA_no);
+		
+		QnADTO dto = null;
+		dto = dao.detail( QnA_no);
+		return dto;
+	}
+ 
+	public int write( QnADTO dto) {
+		int successCount = 0;
+		successCount = dao.write( dto );
+		return successCount;
+	}
+	
+	public List<QnADTO> searchList( SearchDTO dto) {
+		List<QnADTO> list= null;
+		list = dao.searchList( dto);
+		return list;
+	}
+	
+	public int searchListCount( SearchDTO dto) {
+		int totalCount = 0;
+		totalCount = dao.searchListCount( dto );
+		return totalCount;
+	}
+
+
+
+
 
 }
+
+
+
+
+
+
+
+
+
